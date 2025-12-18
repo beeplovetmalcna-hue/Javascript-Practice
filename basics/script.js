75f17c909f2222939e63a9c398ff0f600a5bed39
Biplove <beeplovetmalcna@gmail.com>
2025-12-18 22:44:19 +0545

    Added intial HTML and JS practice files

---
+++ basics/script.js
@@ -0,0 +1,25 @@
+// while loop
+// let i=1;
+// while(i<=5){
+// console.log("i=", i);
+// i++;
+// }
+
+//Calculate sum of 1 to 5 using for loop
+
+// let sum=0;
+// for(let i=1;i<=5;i++){
+//     sum=sum+i;
+// }
+// console.log("sum=",sum);
+
+// Get user to input a number using prompt("Enter a number"). Check if the number is multiple of 5 or not.
+
+let num= prompt("Enter a number");
+if(num%5==0){
+    console.log("The number is multiple of 5");
+}
+else{
+    console.log("The number is not a multiple of 5");
+}
+console.log("num");
